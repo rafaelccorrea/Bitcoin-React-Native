62813066a10c739ccc9360dfcd7a53ba72ec1f7b
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-02-16 00:52:42 -0300

    Separando Style Main

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { Text, View, SafeAreaView, StatusBar } from 'react-native';
 import CurrentPrice from "./src/components/CurrentPrice"
 import HistoryGraphic from "./src/components/HistoryGraphic"
 import QuotationsList from "./src/components/QuotationsList"
 import QuotationsItems from "./src/components/QuotationsList/QuotationsItem"
+import styles from "./styles"
 
 function addNumber0(number){
   if(number <= 9){
@@ -36,12 +37,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    paddingTop: Platform.OS === 'android' ? 40 : 0
-  },
-});
---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000000',
+      alignItems: 'center',
+      paddingTop: Platform.OS === 'android' ? 40 : 0
+    },
+  });
+
+export default styles;
